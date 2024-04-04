6eb981adf2bcd72c2d659e35059d558b5669e763
HaseebullahMiakhil <tw773029@gmail.com>
2024-04-04 16:39:57 +0430

    java steps

--- 1- hello world/hello.java
+++ 1- hello world/hello.java
@@ -20,7 +20,46 @@ public class hello {
         //comments in java
         /* you can use like that comment */   // or you can use like that comment.
 
-        
+        //variables in java
+        String name = "my home";
+        System.out.println(name);
+
+        int mynum = 15;
+        System.out.println(mynum);
+
+        float myfloat = 12.5f;
+        System.out.println(myfloat);
+
+        char myletter = 'd';
+        System.out.println(myletter);
+
+        boolean mytrue = true;
+        boolean myflase = false;
+        System.out.println(mytrue);
+        System.out.println(myflase);
+
+        //print vaiables in java
+        String myname = "hasseeb";
+        System.out.println("hello Mr." + myname);
+
+        int x = 5;
+        int y = 6;
+        System.out.println(x + y);
+
+        //Declare Multiple Variable 
+        int a = 2;
+        int b = 3;
+        int c = 7;
+        System.out.println(a + b + c);
+
+        // all java varaibales must be identified with unique names
+
+        //good
+        int minutesperhour = 60;
+
+        //ok but not good for one professional programmer
+        int m = 27;
+
 
     }
 }
