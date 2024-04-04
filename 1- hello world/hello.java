be5547dbf7d72468e403626352ce2e87746a866f
HaseebullahMiakhil <tw773029@gmail.com>
2024-04-04 16:31:16 +0430

    java

---
+++ 1- hello world/hello.java
@@ -0,0 +1,26 @@
+/**
+ * hello
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("this is start of our java codes");
+        
+
+        //learning output
+        System.out.println("Hello world");
+        System.out.println("i am learning Java!");
+        System.out.println("it is awesome");
+
+        //learning numbers in java
+        System.out.println(3+3);
+        System.out.println(3 + 3);
+        System.out.println(2 * 5);
+
+        //comments in java
+        /* you can use like that comment */   // or you can use like that comment.
+
+        
+
+    }
+}
--- hello.java
+++
@@ -1,8 +0,0 @@
-/**
- * hello
- */
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("hello world come to practice together");
-    }
-}
