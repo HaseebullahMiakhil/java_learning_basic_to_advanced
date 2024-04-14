a02f02460abd8f9829251c5d12bf6e06e221ce32
HaseebullahMiakhil <tw773029@gmail.com>
2024-04-14 16:44:34 +0430

    switch java

--- 1- hello world/hello.java
+++ 1- hello world/hello.java
@@ -160,5 +160,32 @@ public class hello {
         }else{
             System.out.println("good evening");
         }
+        //---------java Switch-------------------//
+        int day = 6;
+        switch (day) {
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                break;
+        }
     }
 }
